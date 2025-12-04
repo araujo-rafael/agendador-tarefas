e3fc4a186a6128a4cf3d0d2b8ef66f3b2a218685
Rafael <rafael12silva73@gmail.com>
2025-12-04 16:13:42 -0300

    adicionado metodo post

---
+++ src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
@@ -0,0 +1,33 @@
+package com.javanauta.agendadortarefas.business;
+
+import com.javanauta.agendadortarefas.business.dto.TarefasDTO;
+import com.javanauta.agendadortarefas.business.mapper.TarefasConverter;
+import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
+import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.javanauta.agendadortarefas.infrastructure.repository.TarefasRepository;
+import com.javanauta.agendadortarefas.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TarefasService {
+
+    private final TarefasRepository tarefasRepository;
+    private final TarefasConverter tarefasConverter;
+    private final JwtUtil jwtUtil;
+
+    public TarefasDTO gravarTarefa(String token, TarefasDTO dto){
+        String email = jwtUtil.extractEmailToken(token.substring(7));
+        dto.setDataCriacao(LocalDateTime.now());
+        dto.setStatusNotificacaoEnum(StatusNotificacaoEnum.PENDENTE);
+        dto.setEmailUSuario(email);
+        TarefasEntity entity = tarefasConverter.paraTarefaEntity(dto);
+
+
+        return tarefasConverter.paraTarefaDTO(
+                tarefasRepository.save(entity));
+    }
+}
